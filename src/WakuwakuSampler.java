b21ae255adbc6875bdc17f2b15709a4dd5bed69b
Shun Shiramatsu <siramatu@nitech.ac.jp>
2018-02-04 10:13:14 +0900

    run video capture

--- src/WakuwakuSampler.java
+++ src/WakuwakuSampler.java
@@ -2,23 +2,34 @@ import javax.sound.sampled.*;
 import java.io.*;
 import java.nio.*;
 import java.util.Arrays;
+import java.util.*;
+import javax.sound.midi.*;
 
 public class WakuwakuSampler implements Runnable {
 	AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
 	File wavFile;
 	
-	static final float SAMPLE_RATE = 44100;
+	static final float SAMPLE_RATE = 22050;
 	static final int SAMPLE_SIZE_IN_BITS = 16;
 	static final int CHANNELS = 1;
 	static final boolean SIGNED = true;
 	static final boolean BIG_ENDIAN = true;
 	
+	static int BPM = 120;
+	
 	static int lowTh = 200;
 	static int HighTh = 1500;
+	static int WindowSize = 4096;
 	
 	TargetDataLine line;
 	String fileName;
 	AudioFormat format;
+	double maxAmp = 0;
+	double normalizeRatio;
+	
+	VideoCapture cap;
+	
+	List<Double> amps, lowAmps, highAmps;
 
 	double[] valuesActual, valuesImaginal;
 
@@ -30,6 +41,7 @@ public class WakuwakuSampler implements Runnable {
 		format = new AudioFormat(SAMPLE_RATE, SAMPLE_SIZE_IN_BITS, CHANNELS, SIGNED, BIG_ENDIAN);
         line = AudioSystem.getTargetDataLine(format);
         line.open(format);
+        cap = new VideoCapture();
         
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         
@@ -37,17 +49,32 @@ public class WakuwakuSampler implements Runnable {
         in.readLine();
         Thread thread = new Thread(this);
         thread.start();
+        Thread capThread = new Thread(cap);
+        capThread.start();
+        
          
         System.out.println("Press Enter to STOP Recording...");
         in.readLine();
         stopRecording();
-        
+        cap.pause();
         
         initialize();
-        dft(2048);
+        dft(WindowSize);
         
+		//int max = (int)Math.pow(2, SAMPLE_SIZE_IN_BITS) * WindowSize;
+		//System.out.println("ratio: "+ (maxAmp / max));
+		
+        //byte[] bassDrumBuf = extractBassDrum();
+        //byte[] snareDrumBuf = extractSnareDrum();
+        //byte[] voiceBuf = extractVoice();
         
 	}
+	
+	/*
+	byte[] extractBassDrum() {
+		for (int i = 0; i < )
+	}
+	*/
 	
 	public void startRecording() {
 		try {
@@ -72,7 +99,12 @@ public class WakuwakuSampler implements Runnable {
 	}
 	
 	void dft(int windowSize) {
+		amps = new ArrayList<Double>();
+		lowAmps = new ArrayList<Double>();
+		highAmps = new ArrayList<Double>();
 		for (int i = 0; i < valuesActual.length; i += windowSize) {
+			double t = iToTime(i);
+			System.out.println("t = "+t+" [sec]");
 			if (valuesActual.length - i >= windowSize) { 
 				double[] spectrumActual = new double[ windowSize ];
 				double[] spectrumImaginal = new double[ windowSize ];
@@ -96,113 +128,137 @@ public class WakuwakuSampler implements Runnable {
 						highAmp += norm;
 					}
 				}
+				amps.add(amp);
+				lowAmps.add(lowAmp);
+				highAmps.add(highAmp);
+				if (amp > maxAmp) { maxAmp = amp; }
 				
 			}
 		}
 	}
 	
+	double iToTime(int i) {
+		return i / format.getSampleRate();
+	}
+	
+	void readMidi(String midiFileName) throws Exception {
+	    Track[] tracks = MidiSystem.getSequence(new File(midiFileName)).getTracks();
+	}
 	
 	/**
-     * —£Uƒt[ƒŠƒG•ÏŠ· http://krr.blog.shinobi.jp/javafx_praxis/java%E3%81%A7%E5%91%A8%E6%B3%A2%E6%95%B0%E5%88%86%E6%9E%90%E3%82%92%E3%81%97%E3%81%A6%E3%81%BF%E3%82%8B
-     * @param in ƒt[ƒŠƒG•ÏŠ·‚ğs‚¤À””z—ñ
-     * @param outActual ŒvZŒ‹‰Ê‚ÌÀ”•””z—ñ
-     * @param outImaginal ŒvZŒ‹‰Ê‚Ì‹•”•””z—ñ
-     * @param winFlg ‘‹ŠÖ”‚Ìg—pƒtƒ‰ƒO
+     * ï¿½ï¿½ï¿½Uï¿½tï¿½[ï¿½ï¿½ï¿½Gï¿½ÏŠï¿½ http://krr.blog.shinobi.jp/javafx_praxis/java%E3%81%A7%E5%91%A8%E6%B3%A2%E6%95%B0%E5%88%86%E6%9E%90%E3%82%92%E3%81%97%E3%81%A6%E3%81%BF%E3%82%8B
+     * @param in ï¿½tï¿½[ï¿½ï¿½ï¿½Gï¿½ÏŠï¿½ï¿½ï¿½ï¿½sï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½zï¿½ï¿½
+     * @param outActual ï¿½vï¿½Zï¿½ï¿½ï¿½Ê‚Ìï¿½ï¿½ï¿½ï¿½ï¿½ï¿½zï¿½ï¿½
+     * @param outImaginal ï¿½vï¿½Zï¿½ï¿½ï¿½Ê‚Ì‹ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½zï¿½ï¿½
+     * @param winFlg ï¿½ï¿½ï¿½Öï¿½ï¿½Ìgï¿½pï¿½tï¿½ï¿½ï¿½O
      */
     void dft( double[] in , double[] outActual , double[] outImaginal , boolean winFlg )
     {
-        // ”z—ñ‰Šú‰»
+        // ï¿½zï¿½ñ‰Šï¿½ï¿½ï¿½
         int  length             = in.length;
          
-        // —£Uƒt[ƒŠƒG•ÏŠ·
+        // ï¿½ï¿½ï¿½Uï¿½tï¿½[ï¿½ï¿½ï¿½Gï¿½ÏŠï¿½
         for( int k=0 ; k<length ; k++ )
         {
-            // ‰Šú‰»
+            // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
             outActual[k]    = 0.0d;
             outImaginal[k]  = 0.0d;
              
-            // ŒvZ
+            // ï¿½vï¿½Z
             for( int n=0 ; n<length ; n++ )
             {
-                // “ü—Í’l‚É‘‹ŠÖ”‚ğ“K—p
+                // ï¿½ï¿½ï¿½Í’lï¿½É‘ï¿½ï¿½Öï¿½ï¿½ï¿½Kï¿½p
                 double normal   = ( !winFlg )? in[n]  : hanWindow( in[n] , n , 0 , length );
                  
-                // kŸ‚ü”g¬•ª‚ğŒvZ
+                // kï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½gï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½vï¿½Z
                 outActual[k]    +=        normal * Math.cos( 2.0 * Math.PI * (double)n * (double)k / (double)length );
                 outImaginal[k]  += -1.0 * normal * Math.sin( 2.0 * Math.PI * (double)n * (double)k / (double)length );
             }
              
-            // c‚è‚ÌŒvZ
+            // ï¿½cï¿½ï¿½ÌŒvï¿½Z
             //outActual[k]    /= length;
             //outImaginal[k]  /= length;
         }
     }
     
     /**
-     * ‘‹ŠÖ”iƒnƒ“‘‹j
-     * @param in •ÏŠ·‚·‚é’l
-     * @param i ”z—ñ’†‚ÌƒCƒ“ƒfƒbƒNƒX
-     * @param minIndex ”z—ñ‚ÌÅ¬ƒCƒ“ƒfƒbƒNƒX
-     * @param maxIndex ”z—ñ‚ÌÅ‘åƒCƒ“ƒfƒbƒNƒX
+     * ï¿½ï¿½ï¿½Öï¿½ï¿½iï¿½nï¿½ï¿½ï¿½ï¿½ï¿½j
+     * @param in ï¿½ÏŠï¿½ï¿½ï¿½ï¿½ï¿½l
+     * @param i ï¿½zï¿½ñ’†‚ÌƒCï¿½ï¿½ï¿½fï¿½bï¿½Nï¿½X
+     * @param minIndex ï¿½zï¿½ï¿½ÌÅï¿½ï¿½Cï¿½ï¿½ï¿½fï¿½bï¿½Nï¿½X
+     * @param maxIndex ï¿½zï¿½ï¿½ÌÅ‘ï¿½Cï¿½ï¿½ï¿½fï¿½bï¿½Nï¿½X
      * @return
      */
     protected double hanWindow( double in , double i , double minIndex , double maxIndex )
     {
-        // “ü—Í’l‚Ì³‹K‰»
+        // ï¿½ï¿½ï¿½Í’lï¿½Ìï¿½ï¿½Kï¿½ï¿½
         double normal   = i / ( maxIndex - minIndex );
-         
-        // ƒnƒ“‘‹ŠÖ”‚Ì’l‚ğæ“¾
+        // ï¿½nï¿½ï¿½ï¿½ï¿½ï¿½Öï¿½ï¿½Ì’lï¿½ï¿½ï¿½æ“¾
         double  han     =  0.5 - 0.5 * Math.cos( 2.0 * Math.PI * normal );
-         
         return in * han;
     }
     
    
     /**
-     * ‰¹ºƒtƒ@ƒCƒ‹‚ğ“Ç‚İ‚İAƒƒ^î•ñ‚ÆƒTƒ“ƒvƒŠƒ“ƒOEƒf[ƒ^‚ğæ“¾
+     * ï¿½ï¿½ï¿½ï¿½ï¿½tï¿½@ï¿½Cï¿½ï¿½ï¿½ï¿½Ç‚İï¿½ï¿½İAï¿½ï¿½ï¿½^ï¿½ï¿½ï¿½ÆƒTï¿½ï¿½ï¿½vï¿½ï¿½ï¿½ï¿½ï¿½Oï¿½Eï¿½fï¿½[ï¿½^ï¿½ï¿½ï¿½æ“¾
      * @throws Exception
      */
     protected void initialize() throws Exception
     {
-        // ‰¹ºƒXƒgƒŠ[ƒ€‚ğæ“¾
+        // ï¿½ï¿½ï¿½ï¿½ï¿½Xï¿½gï¿½ï¿½ï¿½[ï¿½ï¿½ï¿½ï¿½ï¿½æ“¾
         File                file    = new File( fileName );
         AudioInputStream    is      = AudioSystem.getAudioInputStream( file );
          
-        // ƒƒ^î•ñ‚Ìæ“¾
+        // ï¿½ï¿½ï¿½^ï¿½ï¿½ï¿½Ìæ“¾
         System.out.println( format.toString() );
          
-        // æ“¾‚·‚é•W–{”‚ğŒvZ
-        // 1•bŠÔ‚Åæ“¾‚µ‚½•W–{”‚ªƒTƒ“ƒvƒ‹ƒŒ[ƒg‚Å‚ ‚é‚±‚Æ‚©‚çŒvZ
+        // ï¿½Wï¿½{ï¿½ï¿½
         int mount = (int)is.getFrameLength();
-         
-        // ‰¹ºƒf[ƒ^‚Ìæ“¾
+        
+        // ï¿½ï¿½ï¿½ï¿½ï¿½fï¿½[ï¿½^ï¿½Ìæ“¾
         valuesActual    = new double[ mount ];
         valuesImaginal  = new double[ mount ];
-        for( int i=0 ; i<mount ; i++ )
-        {
-            // 1•W–{•ª‚Ì’l‚ğæ“¾
+        double max = 0;
+        for ( int i=0 ; i<mount ; i++ ) {
+            // 1ï¿½Wï¿½{ï¿½ï¿½ï¿½Ì’lï¿½ï¿½ï¿½æ“¾
             int     size        = format.getFrameSize();
             byte[]  data        = new byte[ size ];
             int     readedSize  = is.read(data);
              
-            // ƒf[ƒ^I—¹‚Åƒ‹[ƒv‚ğ”²‚¯‚é
+            // ï¿½fï¿½[ï¿½^ï¿½Iï¿½ï¿½ï¿½Åƒï¿½ï¿½[ï¿½vï¿½ğ”²‚ï¿½ï¿½ï¿½
             if( readedSize == -1 ){ break; } 
              
-            // 1•W–{•ª‚Ì’l‚ğæ“¾
+            // 1ï¿½Wï¿½{ï¿½ï¿½ï¿½Ì’lï¿½ï¿½ï¿½æ“¾
             switch( format.getSampleSizeInBits() )
             {
                 case 8:
                     valuesActual[i]   = (int) data[0];
                     break;
                 case 16:
-                    valuesActual[i]   = (int) ByteBuffer.wrap( data ).order( ByteOrder.LITTLE_ENDIAN ).getShort();
+                	valuesActual[i]   = (int) ByteBuffer.wrap( data ).order( ByteOrder.LITTLE_ENDIAN ).getShort();
+                	/*
+                	if (BIG_ENDIAN) {
+                		valuesActual[i]   = (int) ByteBuffer.wrap( data ).order( ByteOrder.BIG_ENDIAN ).getShort();
+                	} else {
+                		valuesActual[i]   = (int) ByteBuffer.wrap( data ).order( ByteOrder.LITTLE_ENDIAN ).getShort();
+                	}
+                	*/
                     break;
                 default:
             }
+            if(Math.abs(valuesActual[i]) > max) {
+            	max = Math.abs(valuesActual[i]);
+            }
         }
          
-        // ‰¹ºƒXƒgƒŠ[ƒ€‚ğ•Â‚¶‚é
+        // ï¿½ï¿½ï¿½ï¿½ï¿½Xï¿½gï¿½ï¿½ï¿½[ï¿½ï¿½ï¿½ï¿½Â‚ï¿½ï¿½ï¿½
         is.close();
+        
+        normalizeRatio = Math.pow(2, format.getSampleSizeInBits()-1) * 0.9 / max;
+        //System.err.println("normalizeRatio: "+normalizeRatio);
+        for ( int i=0 ; i<mount ; i++ ) {
+        	valuesActual[i] *= normalizeRatio;
+        }
     }
 }
 
